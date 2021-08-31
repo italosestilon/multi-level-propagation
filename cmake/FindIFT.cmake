42e32cd7661fbe0baa0015c79098f75f77437e3c
Italos Estilon <italosestilon@gmail.com>
2021-08-31 11:52:22 -0300

    setting things up

---
+++ cmake/FindIFT.cmake
@@ -0,0 +1,27 @@
+include(FindPackageHandleStandardArgs)
+
+find_library(IFT_LIBRARY
+    NAMES ift
+    PATHS ${LIBIFT_ROOT}/lib
+    NO_DEFAULT_PATH)
+
+message(STATUS "LIBIFT_ROOT: ${LIBIFT_ROOT}/lib")
+message(STATUS "IFT_LIBRARY: ${IFT_LIBRARY}")
+    
+# SET(IFT_LIBRARIES)
+set(IFT_INCLUDE_DIRS ${LIBIFT_ROOT}/include)
+
+file(GLOB IFT_EXTERNAL
+        ${LIBIFT_ROOT}/externals/*/)
+
+foreach(lib ${IFT_EXTERNAL})
+    if(EXISTS ${lib}/src)
+        message(STATUS ${lib};)
+        list(APPEND IFT_EXTERNAL_INCLUDE ${lib}/include)
+        list(APPEND IFT_EXTERNAL_LIB ${lib}/lib)
+    endif()
+endforeach()
+
+SET (IFT_INCLUDE_DIRS ${IFT_INCLUDE_DIRS} ${IFT_EXTERNAL_INCLUDE})
+
+find_package_handle_standard_args(IFT REQUIRED_VARS IFT_LIBRARY IFT_INCLUDE_DIRS)
---
+++ label-propagation/label_propagation.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <label_propagation.h>
+
+void say_hello(){
+    std::cout << "Hello, from label-propagation!\n";
+}
